a0f10574d6a4352d96121a0f23cc75c2eb3b9cdf
Ngô Minh Đức <ducnm@mioapp.vn>
2023-04-14 22:57:33 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lucky-wheel',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
